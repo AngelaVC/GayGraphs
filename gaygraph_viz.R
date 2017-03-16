07fe95c0c24a2b75df2cdfb1e627891864b9a5bb
Angela Vierling-Claassen <angela.the.vc@gmail.com>
2017-03-15 21:03:55 -0400

    fix graph viz

--- gaygraph_viz.R
+++ gaygraph_viz.R
@@ -7,7 +7,7 @@ library('cluster')
 library('animation')
 
 # Set names for trial -- change this each time
-trialname='for-talk-data-2011-12-20-10-45'
+trialname='for-talk-data-2011-12-30-9-17'
 
 # Set directory which contains files -- change as needed
 setwd("/Users/angelavierling-claassen/Documents/DataScience/GayGraphs_data/")
@@ -19,8 +19,8 @@ start_end='end'
 
 # read in the graph edges data and the nodes data
 # change filenames as needed 
-gaygraph_data_frame<-read.table(cat(trialname,'-edges-',start_end,sep=""))
-nodes<-read.table(cat(trialname,'nodes-',start_end,sep=""))
+gaygraph_data_frame<-read.table(paste(trialname,'-edges-',start_end,'.txt',sep=""))
+nodes<-read.table(paste(trialname,'-nodes-',start_end,'.txt',sep=""))
 
 # Make the names of columns for edges and notes
 colnames(gaygraph_data_frame) <- c('ego', 'alter', 'family', 'friend')
@@ -35,7 +35,7 @@ gaygraph_nonzero_edges <- subset(gaygraph_data_frame,(family > 0 | friend > 0 ))
 gaygraph <- as.undirected(graph.data.frame(d=gaygraph_nonzero_edges,vertices=nodes), mode='collapse')
 
 # do walktrap algorithm to determine communities 
-community_wt <- walktrap.community(gaygraph, steps=4,modularity=TRUE,labels=TRUE)
+community_wt <- walktrap.community(gaygraph, steps=4,modularity=TRUE)
 
 # Simple graph with a layout, vertex colors
 fullGraph_layout<- layout.kamada.kawai(gaygraph)
@@ -62,7 +62,7 @@ V(gaygraph)$color = gay_vertex_colors
 V(gaygraph)$frame = gay_vertex_colors
 
 # Plot the graph
-jpeg(cat(trialname,'-',start_end,'.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'.jpg',sep=""))
 plot(gaygraph, 
      layout=fullGraph_layout, 
      vertex.color=gay_vertex_colors, 
@@ -75,49 +75,49 @@ dev.off()
 gay_only_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "gay")==0])
 gay_only_graph
 gay_layout <- layout.fruchterman.reingold(gay_only_graph)
-jpeg(cat(trialname,'-',start_end,'-gayonly','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-gayonly','.jpg',sep=""))
 plot(gay_only_graph, layout=gay_layout)
 dev.off()
 
 ################################
 ## Look at subgraph of same number of straight nodes
-straight_only_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "gay")==1])
-straight_only_graph<- delete.vertices(straight_only_graph, V(straight_only_graph)[1:length(V(straight_only_graph))-length(V(gay_only_graph))])
-jpeg(cat(trialname,'-',start_end,'-straightonly','.jpg',,sep=""))
-straight_layout <- layout.fruchterman.reingold(straight_only_graph)
-plot(straight_only_graph, layout=straight_layout)
-dev.off()
+#straight_only_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "gay")==1])
+#straight_only_graph<- delete.vertices(straight_only_graph, V(straight_only_graph)[1:length(V(straight_only_graph))-length(V(gay_only_graph))])
+#jpeg(paste(trialname,'-',start_end,'-straightonly','.jpg',sep=""))
+#straight_layout <- layout.fruchterman.reingold(straight_only_graph)
+#plot(straight_only_graph, layout=straight_layout)
+#dev.off()
 
 
 ################################3
 ## Next try for graphs of communities
 comm0_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=0])
 community_layout <- layout.fruchterman.reingold(comm0_graph)
-jpeg(cat(trialname,'-',start_end,'-comm0','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm0','.jpg',sep=""))
 	plot(comm0_graph, layout=community_layout)
 	dev.off()
 
 comm1_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=1])
 community_layout <- layout.fruchterman.reingold(comm1_graph)
-jpeg(cat(trialname,'-',start_end,'-comm1','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm1','.jpg',sep=""))
 	plot(comm1_graph, layout=community_layout)
 	dev.off()
 
 comm2_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=2])
 community_layout <- layout.fruchterman.reingold(comm2_graph)
-jpeg(cat(trialname,'-',start_end,'-comm2','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm2','.jpg',sep=""))
 	plot(comm2_graph, layout=community_layout)
 	dev.off()
 
 comm3_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=3])
 community_layout <- layout.fruchterman.reingold(comm3_graph)
-jpeg(cat(trialname,'-',start_end,'-comm3','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm3','.jpg',sep=""))
 	plot(comm3_graph, layout=community_layout)
 	dev.off()
 
 comm4_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=3])
 community_layout <- layout.fruchterman.reingold(comm4_graph)
-jpeg(cat(trialname,'-',start_end,'-comm4','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm4','.jpg',sep=""))
 	plot(comm4_graph, layout=community_layout)
 	dev.off()
 
@@ -128,8 +128,8 @@ start_end='start'
 
 # read in the graph edges data and the nodes data
 # change filenames as needed 
-gaygraph_data_frame<-read.table(cat(trialname,'-edges-',start_end,sep=""))
-nodes<-read.table(cat(trialname,'nodes-',start_end,sep=""))
+gaygraph_data_frame<-read.table(paste(trialname,'-edges-',start_end,sep=""))
+nodes<-read.table(paste(trialname,'nodes-',start_end,sep=""))
 
 # Make the names of columns for edges and notes
 colnames(gaygraph_data_frame) <- c('ego', 'alter', 'family', 'friend')
@@ -171,7 +171,7 @@ V(gaygraph)$color = gay_vertex_colors
 V(gaygraph)$frame = gay_vertex_colors
 
 # Plot the graph
-jpeg(cat(trialname,'-',start_end,'.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'.jpg',sep=""))
 plot(gaygraph, 
      layout=fullGraph_layout, 
      vertex.color=gay_vertex_colors, 
@@ -184,49 +184,49 @@ dev.off()
 gay_only_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "gay")==0])
 gay_only_graph
 gay_layout <- layout.fruchterman.reingold(gay_only_graph)
-jpeg(cat(trialname,'-',start_end,'-gayonly','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-gayonly','.jpg',sep=""))
 plot(gay_only_graph, layout=gay_layout)
 dev.off()
 
 ################################
 ## Look at subgraph of same number of straight nodes
-straight_only_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "gay")==1])
-straight_only_graph<- delete.vertices(straight_only_graph, V(straight_only_graph)[1:length(V(straight_only_graph))-length(V(gay_only_graph))])
-jpeg(cat(trialname,'-',start_end,'-straightonly','.jpg',,sep=""))
-straight_layout <- layout.fruchterman.reingold(straight_only_graph)
-plot(straight_only_graph, layout=straight_layout)
-dev.off()
+#straight_only_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "gay")==1])
+#straight_only_graph<- delete.vertices(straight_only_graph, V(straight_only_graph)[1:length(V(straight_only_graph))-length(V(gay_only_graph))])
+#jpeg(paste(trialname,'-',start_end,'-straightonly','.jpg',sep=""))
+#straight_layout <- layout.fruchterman.reingold(straight_only_graph)
+#plot(straight_only_graph, layout=straight_layout)
+#dev.off()
 
 
 ################################3
 ## Next try for graphs of communities
 comm0_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=0])
 community_layout <- layout.fruchterman.reingold(comm0_graph)
-jpeg(cat(trialname,'-',start_end,'-comm0','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm0','.jpg',sep=""))
 	plot(comm0_graph, layout=community_layout)
 	dev.off()
 
 comm1_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=1])
 community_layout <- layout.fruchterman.reingold(comm1_graph)
-jpeg(cat(trialname,'-',start_end,'-comm1','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm1','.jpg',sep=""))
 	plot(comm1_graph, layout=community_layout)
 	dev.off()
 
 comm2_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=2])
 community_layout <- layout.fruchterman.reingold(comm2_graph)
-jpeg(cat(trialname,'-',start_end,'-comm2','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm2','.jpg',sep=""))
 	plot(comm2_graph, layout=community_layout)
 	dev.off()
 
 comm3_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=3])
 community_layout <- layout.fruchterman.reingold(comm3_graph)
-jpeg(cat(trialname,'-',start_end,'-comm3','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm3','.jpg',sep=""))
 	plot(comm3_graph, layout=community_layout)
 	dev.off()
 
 comm4_graph <- delete.vertices(gaygraph, V(gaygraph)[get.vertex.attribute(gaygraph,name = "membership")!=3])
 community_layout <- layout.fruchterman.reingold(comm4_graph)
-jpeg(cat(trialname,'-',start_end,'-comm4','.jpg',,sep=""))
+jpeg(paste(trialname,'-',start_end,'-comm4','.jpg',sep=""))
 	plot(comm4_graph, layout=community_layout)
 	dev.off()
 
